142476d37a53056b45a5b58bf63c0eb6200c08bc
YouriT <ytolstoy@gmail.com>
2012-12-04 11:19:45 +0100

    Initial Commit

---
+++ control-tower/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  control-tower
+//
+//  Created by Youri Tolstoy on 4/12/12.
+//  Copyright (c) 2012 Youri Tolstoy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
