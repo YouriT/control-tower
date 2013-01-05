9a8e139b28c8422e90176a53e592cfc5efb15403
GregB <itachi42@gmail.com>
2013-01-05 18:48:55 +0100

    tentative de loop 6

--- src/atis.c
+++ src/atis.c
@@ -38,11 +38,11 @@ int main(int argc, const char * argv[])
         exit(EXIT_FAILURE);
     }
 
-    char meteo[48];
+    char meteo[40];
     int i = 0;
     int j;
     srand((int)(time(NULL)));
-    while (i < 35)
+    while (i < 40)
     {
         j = 0;
         while (j < 5)
--- src/control-tower.c
+++ src/control-tower.c
@@ -49,7 +49,6 @@ int main(int argc, const char * argv[])
         int i = 0;
         while ((fread(&c, sizeof(char), 1, atisFile) > 0) && i < MAX_BUF)
         {
-            printf("test %d",i);
             atis.content[i] = c;
             i++;
         }
